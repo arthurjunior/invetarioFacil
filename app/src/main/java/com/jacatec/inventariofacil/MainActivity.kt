6a298a629407677f974eae4ae312cacb255e9b49
Arthur Junior <dev.arthurcarvalho@gmail.com>
2024-01-24 13:51:53 -0400

    configurar tempo de splash screen ao abrir o app

--- app/src/main/java/com/jacatec/inventariofacil/MainActivity.kt
+++ app/src/main/java/com/jacatec/inventariofacil/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jacatec.inventariofacil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Classe para configurar tempo de splash screen ao abrir o app
-        Handler(Looper.getMainLooper()).postDelayed({},3000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,Home::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
